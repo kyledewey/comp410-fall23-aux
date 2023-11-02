52182f903cb850a27a79e2ca00bb892ee5898b42
Kyle Dewey <kyle.dewey@csun.edu>
2023-11-01 18:14:18 -0700

    code from class

--- gen.pl
+++ gen.pl
@@ -4,9 +4,24 @@ genOp(mult_op).
 genOp(div_op).
 
 % bounded-exhaustive search
-genExp(integerExp(Int)) :-
+% genExp: Bound (Int), Exp
+genExp(_, integerExp(Int)) :-
     between(0, 2, Int).
-genExp(opExp(Left, Op, Right)) :-
-    genExp(Left),
+genExp(Bound, opExp(Left, Op, Right)) :-
+    Bound > 0,
+    NewBound is Bound - 1,
+    genExp(NewBound, Left),
     genOp(Op),
-    genExp(Right).
+    genExp(NewBound, Right).
+
+dividesByZero(opExp(_, div_op, integerExp(0))).
+
+    
+exp(_, true).
+exp(_, false).
+exp(N, if(E1, E2, E3)) :-
+    N > 0,
+    N1 is N - 1,
+    exp(N1, E1),
+    exp(N1, E2),
+    exp(N1, E3).
